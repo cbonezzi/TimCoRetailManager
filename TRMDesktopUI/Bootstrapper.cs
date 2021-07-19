83ab6157205de17960bd253f339e1b7d22dbb7b2
Cesar Bonezzi <cbonezzi@ea.com>
2021-07-18 20:46:07 -0500

    implemented dependency injection and added a shellview and shellviewmodel MVVM

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -1,20 +1,62 @@
-﻿using System.Windows;
-using Caliburn.Micro;
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using TRMDesktopUI.ViewModels;
 
 namespace TRMDesktopUI
 {
 	public class Bootstrapper : BootstrapperBase
 	{
+		private readonly SimpleContainer _container = new SimpleContainer();
+
 		public Bootstrapper()
 		{
 			Initialize();
+		}
+
+		protected override void Configure()
+		{
+			_container.Instance(_container);
+
+			//central location for both windowmanager and eventaggregator
+			//this will keep a central location for these important info
+			//know it all for this for this instance.
+			_container
+				.Singleton<IWindowManager, WindowManager>()
+				.Singleton<IEventAggregator, EventAggregator>();
+
+			//using reflection at this point
+			//this is fine for now, but for the future, instead of wiring directly to class, we can
+			//do the interface.
+			GetType().Assembly.GetTypes()
+				.Where(type => type.IsClass)
+				.Where(type => type.Name.EndsWith("ViewModel"))
+				.ToList()
+				.ForEach(viewModelType => _container.RegisterPerRequest(
+					viewModelType, viewModelType.ToString(), viewModelType));
 		}
 
 		protected override void OnStartup(object sender, StartupEventArgs e)
 		{
 			//launch shellviewmodel as our startup
 			DisplayRootViewFor<ShellViewModel>();
+		}
+
+		protected override object GetInstance(Type service, string key)
+		{
+			return _container.GetInstance(service, key);
+		}
+
+		protected override IEnumerable<object> GetAllInstances(Type service)
+		{
+			return _container.GetAllInstances(service);
+		}
+
+		protected override void BuildUp(object instance)
+		{
+			_container.BuildUp(instance);
 		}
 	}
 }
