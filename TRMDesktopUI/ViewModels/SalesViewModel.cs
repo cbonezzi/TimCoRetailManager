a3671fe690ab49bcce80faf7c256b3156a9fb520
Cesar Bonezzi <cbonezzi@ea.com>
2021-07-20 00:34:18 -0500

    adding salesviewmodel. adding the salesview. initial stage for the wiring of the salesview to the salesviewmodel.

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace TRMDesktopUI.ViewModels
+{
+	public class SalesViewModel : Screen
+	{
+		private BindingList<string> _products;
+
+		public BindingList<string> Products
+		{
+			get { return _products; }
+			set
+			{
+				_products = value;
+				NotifyOfPropertyChange(()=> Products);
+			}
+		}
+
+		private BindingList<string> _cart;
+
+		public BindingList<string> Cart
+		{
+			get { return _cart; }
+			set
+			{
+				_cart = value;
+				NotifyOfPropertyChange(() => Cart);
+			}
+		}
+
+		public string SubTotal
+		{
+			get
+			{
+				// TODO - replace with calculation
+				return "$0.00";
+			}
+		}
+
+		public string Tax
+		{
+			get
+			{
+				// TODO - replace with calculation
+				return "$0.00";
+			}
+		}
+		public string Total
+		{
+			get
+			{
+				// TODO - replace with calculation
+				return "$0.00";
+			}
+		}
+
+		private string _itemQuantity;
+
+		public string ItemQuantity
+		{
+			get { return _itemQuantity; }
+			set
+			{
+				_itemQuantity = value;
+				NotifyOfPropertyChange(() => ItemQuantity);
+			}
+		}
+
+		public bool AddToCart
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+		public bool CanRemoveFromCart
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+		public void RemoveFromCart()
+		{
+		}
+
+		public bool CanCheckOut
+		{
+			get
+			{
+				bool output = false;
+
+				return output;
+			}
+		}
+
+		public void CheckOut()
+		{
+
+		}
+	}
+}
