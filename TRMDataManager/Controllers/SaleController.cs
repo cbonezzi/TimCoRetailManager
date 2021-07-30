832954bd0d838eef542c47b7b0a3ebd80d513918
Cesar Bonezzi <cbonezzi@ea.com>
2021-07-30 18:51:01 -0500

    add admin enpoint data
    add the data for administrative endpoints
    does not yet secure them

--- TRMDataManager/Controllers/SaleController.cs
+++ TRMDataManager/Controllers/SaleController.cs
@@ -1,26 +1,28 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using Microsoft.AspNet.Identity;
 using TRMDataManager.Library.DataAccess;
 using TRMDataManager.Library.Models;
-using TRMDataManager.Models;
 
 namespace TRMDataManager.Controllers
 {
-    [Authorize]
-    public class SaleController : ApiController
-    {
-	    public void Post(SaleModel sale)
-	    {
-		    SaleData data = new SaleData();
+	[Authorize]
+	public class SaleController : ApiController
+	{
+		public void Post(SaleModel sale)
+		{
+			SaleData data = new SaleData();
 
-		    string userId = RequestContext.Principal.Identity.GetUserId();
+			string userId = RequestContext.Principal.Identity.GetUserId();
 
-		    data.SaveSale(sale, userId);
-	    }
-    }
+			data.SaveSale(sale, userId);
+		}
+
+		[Route("GetSalesReport")]
+		public List<SaleReportModel> GetSalesReport()
+		{
+			SaleData data = new SaleData();
+			return data.GetSaleReport();
+		}
+	}
 }
