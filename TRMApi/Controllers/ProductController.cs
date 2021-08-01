ae00b86811854513688a9d781f6132971166bfb6
Cesar Bonezzi <cbonezzi@ea.com>
2021-08-01 11:06:49 -0500

    add new api and moved over functionality

---
+++ TRMApi/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[Authorize]
+	public class ProductController : ControllerBase
+	{
+		// GET api/values
+		public List<ProductModel> Get()
+		{
+			ProductData data = new ProductData();
+
+			return data.GetProducts();
+		}
+	}
+}
